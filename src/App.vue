105071c9c9f15035d5241bdf3c2d3b935c022ddb
chusehi <yu.nakamura.gamegod@gmail.com>
2023-10-04 18:30:32 +0900

    background-colorとテーマの実装

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,66 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="ja">
+  <div>
+    <h1>CSSで実装する簡易的なwebパーツ</h1>
+    <h2>～これで君も立派なCSSの魔術師～</h2>
+  </div>
+    <button class="button">クリックしてみて！</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { devtools } from 'vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    devtools
+}
 }
 </script>
 
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #474741;
   margin-top: 60px;
 }
+
+@font-face {
+  font-family: "kanazawa";
+  src: url("./fonts/kanazawa-gothic-mini.otf") format("opentype");
+  src: url("./fonts/NotoSansJP-.ttf") format("truetype");
+}
+
+body {
+  background-color: antiquewhite;
+}
+
+h1 {
+  font-size: 40px;
+  font-weight: 700;
+  font-family: 'kanazawa';
+}
+
+h2 {
+  font-size: 30px;
+  font-weight: 545;
+}
+
+/* ボタンのスタイル */
+.button {
+    background-color: #007BFF;
+    color: white;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+}
+
+.button:hover {
+    background-color: #0056b3;
+}
+
 </style>
